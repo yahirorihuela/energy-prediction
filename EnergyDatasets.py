6b7e6c79cd89dd1522582685afea7f2f42e1f1ce
Yahir Orihuela <yorihuela57@gmail.com>
2025-06-24 11:46:25 -0700

    Added funcs to filter columns from a dataframe.

--- EnergyDatasets.py
+++ EnergyDatasets.py
@@ -1,5 +1,21 @@
-import numpy
-import matplotlib
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-print("Hello World")
+def original_dataset(data):
+    Original_Dataset = pd.read_csv(data)
+    return Original_Dataset
+def Selecting_Relevant_Columns(name_of_dataset, list_of_relevant_abbreviations):
+    Original_Dataset = pd.read_csv(name_of_dataset)
+    Original_Columns = list(Original_Dataset)
+    Relevant_Columns = []
+    for Column in Original_Columns:
+        verify_column = any(Column.startswith(r_abbrev) for r_abbrev in list_of_relevant_abbreviations)
+        if verify_column == True:
+            Relevant_Columns.append(Column)
+    return Relevant_Columns
+
+def Filtered_Dataset(old_dataset_name, transferred_columns):
+    new_df = pd.read_csv(old_dataset_name, usecols = transferred_columns)
+    return new_df
+
